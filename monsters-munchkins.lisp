ae8e69e31d9bd699999157abd45fa77c8bd68f21
HarrisonBubbles <harris@bubalo.us>
2021-04-18 12:56:53 -0400

    working algo

--- monsters-munchkins.lisp
+++ monsters-munchkins.lisp
@@ -1,6 +1,13 @@
+; ****************** BEGIN INITIALIZATION FOR ACL2s MODE ****************** ;
+; (Nothing to see here!  Your actual file is after this initialization code);
+(make-event
+ (er-progn
+  (set-deferred-ttag-notes t state)
+  (value '(value-triple :invisible))))
 
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the CCG book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/ccg/ccg" :uncertified-okp nil :dir :system :ttags ((:ccg)) :load-compiled-file nil);v4.0 change
 
-<<<<<<< HEAD
 ;Common base theory for all modes.
 #+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s base theory book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
 (include-book "acl2s/base-theory" :dir :system :ttags :all)
@@ -57,8 +64,6 @@
 ; ******************* END INITIALIZATION FOR ACL2s MODE ******************* ;
 ;$ACL2s-SMode$;ACL2s
 (set-ccg-time-limit nil)
-=======
->>>>>>> 4469f9731e863e4613d4e8ebfac929d1a6a2bb0e
 ;; Represents the number of Monsters
 ;(defdata monsters nat)
 ;; Represents the number of Munchkins
@@ -75,87 +80,38 @@
 ;; Number of Monsters and Munchkins respectively on a river bank
 (defdata count (list nat nat))
 
-<<<<<<< HEAD
-#|
-(definec which-side (lc :count b :side rc :count) :tl
-  (declare (ignorable lc rc))
-  (if (equal b 'left)
-    (alg-left lc b rc)
-    (alg-right lc b rc)))
-|#
+;; data definitions for types of moves
+(defdata move-left (list 'move nat 'monsters 'and nat 'munchkins 'to 'the 'left))
+(defdata move-right (list 'move nat 'monsters 'and nat 'munchkins 'to 'the 'right))
+(defdata move (oneof move-left move-right))
 
-(definec alg-left (lc :count b :side rc :count) :tl
-  (declare (ignorable lc b rc))
-  :ic (and (= (+ (first lc) (first rc)) (+ (second lc) (second rc)))
-           (> (+ (first lc) (first rc) (second lc) (second rc)) 8)
-           (equal b 'left))
-  (move 2 2 'right))
-   ;((= (second lc) 4) (move 0 4 'right))
-   ;((> (first lc) 4) (move 4 0 'right))
-   ;((< (first lc) 4) (move (first lc) 0 'right))))
+; data definitions for types of list of moves
+(defdata lom-start-left (oneof '()
+                               (cons move-right '())
+                               (cons move-right (cons move-left lom-start-left))))
+(defdata lom-start-right (oneof '()
+                                (cons move-left '())
+                               (cons move-left (cons move-right lom-start-right))))
+(defdata lom (oneof lom-start-left lom-start-right))
 
-(definec alg-right (lc :count b :side rc :count) :tl
-  (declare (ignorable lc b rc))
-  :ic (and (= (+ (first lc) (first rc)) (+ (second lc) (second rc)))
-           (> (+ (first lc) (first rc) (second lc) (second rc)) 8)
-           (equal b 'right))
-  (move 1 1 'left))#|ACL2s-ToDo-Line|#
-
-   ;(t (move 1 0 'left))))
-#|
-(definec alg-final (lc :count b :side rc :count) :tl
-  (declare (ignorable lc b rc))
-=======
-;; Capacity and the side the boat is on
-(defdata boat (list nat side))
-
-;; Traditional Algorithm
-
-;; Shouldn't have a function this long, running out of time to prove all of them
-;; Solution: simplify it into even more helper functions
-;; Move left and move right function... try to split off changes into other functions
-
-;; Prints the directions in case that the 
-;; Monsters and Munchkins counts are greater than 4
-(definec helper (lc :count b :boat rc :count) :tl
-           ;; number of munchkins = number of monsters
->>>>>>> 4469f9731e863e4613d4e8ebfac929d1a6a2bb0e
-  :ic (and (= (+ (first lc) (first rc)) (+ (second lc) (second rc)))
-           (> (+ (first lc) (first rc) (second lc) (second rc)) 8)
-<<<<<<< HEAD
-           (and (= (first lc) (second lc))
-                (= (first rc) (second rc))
-                (= (first lc) 4)
-                (= (second lc) 4))
-  (list (move 0 4 'right)
-        (
-|#
-
+; helper function for the algorithm, recursively deals with cases
+; where there are more than 4 starting monsters and munchkins
 (definec alg-help (lc :count b :side rc :count) :tl
+           ;; must have equal monsters and munchkins
   :ic (and (= (+ (first lc) (first rc)) (+ (second lc) (second rc)))
+           ;; must have more than 4 monsters and 4 munchkins
            (> (+ (first lc) (first rc) (second lc) (second rc)) 8)
+           ;; left side must have 4 or more munchkins
            (implies (equal b 'left)
-                    (>= (first lc) 4))
-           (implies (equal b 'right)
-                    (and (> (first rc) 0)
-                         (>= (first lc) 3)))
+                    (>= (second lc) 4))
+           ;; both sides must have an equal amount of monsters and munchkins
+           ;; (case where you move all munchkins over is hardcoded in first
+           ;; branch, so you don't need to account for this in contracts)
            (and (= (first lc) (second lc))
                 (= (first rc) (second rc))))
-           #|
-           (implies (= (second rc) 0)
-                    (equal b 'left))
-           (implies (>= (second lc) 4)
-                    (and (= (first lc) (second lc))
-                         (= (first rc) (second rc))))
-           (implies (and (< (second lc) 4)
-                         (equal b 'left))
-                    (= (second lc) 0))
-           (implies (= (second rc) 0)
-                    (= (first rc) 0))
-           (implies (> (second rc) 0)
-                    (> (first rc) 0)))|#
                     
   (cond
+   ;; hard code last steps, once there are 4 munchkins on left side
    ((and (= (first lc) 4)
          (= (second lc) 4)
          (equal b 'left)) (list (move 0 4 'right)
@@ -163,141 +119,89 @@
                                 (move 4 0 'right)
                                 (move 1 0 'left)
                                 (move 2 0 'right)))
+   ;; recursively bring 2 pairs over, 1 pair back
    ((equal b 'left) (cons (move 2 2 'right)
-                          (alg-help (list (- (first lc) 2)
-                                          (- (second lc) 2))
-                                    'right
-                                    (list (+ (first rc) 2)
-                                          (+ (second rc) 2)))))
-                                          
-   ((equal b 'right) (cons (move 1 1 'left)
-                          (alg-help (list (+ (first lc) 1)
-                                          (+ (second lc) 1))
+                          (cons (move 1 1 'left)
+                          (alg-help (list (- (first lc) 1)
+                                          (- (second lc) 1))
                                     'left
-                                    (list (- (first rc) 1)
-                                          (- (second rc) 1)))))))
+                                    (list (+ (first rc) 1)
+                                          (+ (second rc) 1))))))))
 
-;
-(definec simplealg (lc :count b :side rc :count) :tl
+; algorithm for solving a game of monsters and munchkins
+(definec alg (lc :count b :side rc :count) :tl
   (declare (ignorable b rc))
-  :ic (and (and (= (first rc) 0)
-                (= (second rc) 0))
+           ;; both sides must have an equal amount of monsters and
+           ;; munchkins
+  :ic (and (= (first rc) (second rc))
            (= (first lc) (second lc))
+           ;; boat starts on left when game begins
            (equal b 'left))
-=======
-           ;; if more than 0 munchkins on left side, 
-           ;; there should be more munchkins than monsters on left
-           (implies (not (zp (second lc)))
-             (>= (second lc) (first lc)))
-           ;; if more than 0 munchkins on right side, 
-           ;; there should be more munchkins than monsters on right
-           (implies (not (zp (second rc)))
-             (>= (second rc) (first rc)))
-           ;; if more than 0 munchkins on right side and
-           ;; boat is on the right side, there should be 
-           ;; more munchkins than monsters on right and there
-           ;; should be more than 0 monsters
-           (implies (and (not (zp (second rc)))
-                         (equal (second b) 'right))
-             (and (>= (second rc) (first rc))
-                  (> (first rc) 0)))
-           ;; if 0 munchkins on right side there should 
-           ;; also be 0 monsters on right side
-           (implies (zp (second rc))
-                    (zp (first rc))))
   (cond 
-   ((and (> (second lc) 4)
-         (equal (second b) 'left)) (cons (move 2 2 'right)
-                                         (helper (list (- (first lc) 2) 
-                                                       (- (second lc) 2))
-                                                 (list 4 'right)
-                                                 (list (+ (first rc) 2)
-                                                       (+ (second rc) 2)))))
-   ((and (> (second lc) 0)
-         (equal (second b) 'right)) (cons (move 1 1 'left)
-                                         (helper (list (+ (first lc) 1) 
-                                                       (+ (second lc) 1))
-                                                 (list 4 'left)
-                                                 (list (- (first rc) 1)
-                                                       (- (second rc) 1)))))
-   ((and (= (second lc) 4)
-         (equal (second b) 'left)) (cons (move 0 4 'right)
-                                         (helper (list (first lc)
-                                                       (- (second lc) 4))
-                                                 (list 4 'right)
-                                                 (list (first rc)
-                                                       (+ (second rc) 4)))))
-   ((and (> (first lc) 4)
-         (equal (second b) 'left)) (cons (move 4 0 'right)
-                                         (helper (list (- (first lc) 4)
-                                                       (second lc))
-                                                 (list 4 'right)
-                                                 (list (+ (first rc) 4)
-                                                       (second rc)))))
-   ((and (<= (first lc) 4)
-         (equal (second b) 'left)) (list (move (first lc) 0 'right)))
-   (t (cons (move 1 0 'left)
-            (helper (list (+ (first lc) 1)
-                          (second lc))
-                    (list 4 'left)
-                    (list (- (first rc) 1)
-                          (second rc)))))))
-
-;; find case that is terminating over time
-;; running out of time to terminate
-;; look for thing that is changing over time
-;; measure function, decreasing in every time step
-;; measure function has to decrease every time on the recursive calls
-;; figure out the measure function, closer to base case, base case will run
-;; proof will be a termination proof about helper
-;; termination proof is not enough to prove validity
-
-
-;; correctness proof => if (valid initial state), returns a valid final state
-;; if execute moves, all on the right side
-;; need a way to show that the steps get to the right state
-
-;; 1) function that takes in a list of moves and initial state => final state
-;; 2) (test? '(implies ...)) focus on this part FIRST
-;; 3) then think about prove algo is correct, part of that is defthm
-;; (if (and (s is a valid state) (l is a list of moves from running (helper s))) 
-;;  then the result of simulating l from state s is a valid final state)
-;; (test? '(implies ...))
-
-;; function that simulates moves (way easier)
-;; takes in list of moves and initial state => gives back final state
-;; move blank to blank and then does it, gives final state
-
-#|
-To-Do
-- write a measure function (termination proof is the meat)
-- write a simulator, (initial state and list of moves => execute them properly)
-- (test? '(implies ...))
-- after completion of (test? ...) then reach out to Josh :)
-- no need to broaden the invariants
-|#
-
-;
-;(definec (lc :count b :boat rc :count) :tl
-  ;(
-
-;; Prints the rest of the moves
-(defun tradalg (lc b rc)
->>>>>>> 4469f9731e863e4613d4e8ebfac929d1a6a2bb0e
-  (cond 
+   ;; if no monsters or munchkins, no moves
    ((= (first lc) 0) '())
+   ;; if total monsters and munchkins are less than boat capacity,
+   ;; move them all over
    ((<= (+ (first lc) (second lc)) 4) (move (first lc) (second lc) 'right))
+   ;; hardcoded case with 3 monsters and 3 munchkins
    ((= (first lc) 3) (list (move 2 2 'right)
                            (move 1 1 'left)
                            (move 2 2 'right)))
+   ;; hardcoded case with 4 monsters and 4 munchkins
    ((= (first lc) 4) (list (move 2 2 'right)
                            (move 1 1 'left)
                            (move 2 2 'right)
                            (move 1 1 'left)
                            (move 2 2 'right)))
-   (t (alg-left lc b rc))))
+   ;; if more complicated case, call helper
+   (t (alg-help lc b rc))))
 
+; simulate a single move in the game
+(definec simulate-move (m :move lc :count b :side rc :count) :tl
+  (declare (ignorable b))
+      ; if the boat is on the left side, move should be towards right
+      ; (and vice versa)
+  :ic (if (equal b 'left)
+        (move-rightp m)
+        (move-leftp m))
+  ;; update lc, b, and rc depending on move
+  (if (move-rightp m)
+    (list 
+     (list (- (first lc) (second m))
+           (- (second lc) (fifth m)))
+     'right
+     (list (+ (first rc) (second m))
+           (+ (second rc) (fifth m))))
+    (list
+     (list (+ (first lc) (second m))
+           (+ (second lc) (fifth m)))
+     'left
+     (list (- (first rc) (second m))
+           (- (second rc) (fifth m))))))#|ACL2s-ToDo-Line|#
 
+#|
+(definec check-move (m :move lc :count b :side rc :count) :boolean
+  :ic (if (equal b 'left)
+        (move-rightp m)
+        (move-leftp m))
+  (if (equal b 'left)
+    (and (<= (second m) (first lc))
+         (<= (fifth m) (second lc)))
+    (and (<= (second m) (first rc))
+         (<= (fifth m) (second rc)))))
+|#
+    
+; simulate the execution of a list of moves in a game
+(definec simulate (lom :lom lc :count b :side rc :count) :tl
+  :ic (if (equal b 'left)
+        (lom-start-leftp lom)
+        (lom-start-rightp lom))
+  (cond
+   ((endp lom) (list lc b rc))
+   ((consp lom) (simulate (rest lom)
+                          (first (simulate-move (first lom) lc b rc))
+                          (second (simulate-move (first lom) lc b rc))
+                          (third (simulate-move (first lom) lc b rc))))))
 ;; find case that is terminating over time
 ;; running out of time to terminate
 ;; look for thing that is changing over time
